df640a01fd880366dade0ae8b167a82ca1e638db
AustinAmy <67663377+AustinAmy@users.noreply.github.com>
2020-08-19 13:52:59 -0500

    Update config.js

--- WeddingMuseum/config.js
+++ WeddingMuseum/config.js
@@ -13,7 +13,7 @@ var config = {
             id: 'first-museum',
             title: 'Blanton Museum of Art',
             image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/University_of_Texas_at_Austin_August_2019_41_%28Jack_S._Blanton_Museum_of_Art%29.jpg/220px-University_of_Texas_at_Austin_August_2019_41_%28Jack_S._Blanton_Museum_of_Art%29.jpg',
-            description: 'The Blanton has capacity for 150. Cost starts at $5,000. For more information, call (512) 475-6516. Photo by Michael Barera.',
+            description: 'The Blanton has capacity for 150. Cost starts at $5,000.' +'<b>'+ 'For more information, call (512) 475-6516.' + '</b.>'+ 'Photo by Michael Barera.',
 
             location: {
                 center: [-97.73744, 30.281],
@@ -28,7 +28,7 @@ var config = {
             id: 'second-museum',
             title: 'Jones Center - The Contemporary Austin',
             image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/The_Contemporar_yAustin_-_JonesCenter.jpg/220px-The_Contemporar_yAustin_-_JonesCenter.jpg',
-            description: 'The Jones Center has capacity for 250. Cost starts at $2,900. For more information, call (512) 458-8191 ext. 255. Photo by Laguna Jones.',
+            description: 'The Jones Center has capacity for 250. Cost starts at $2,900.'+'<b>'+'For more information, call (512) 458-8191 ext. 255.'+'</b>'+'Photo by Laguna Jones.',
             location: {
                 center: [-97.74269, 30.26958],
                 zoom: 17,
@@ -42,7 +42,7 @@ var config = {
               id: 'third-museum',
               title: 'Bullock Texas State History Museum',
               image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/BobBullockMuseumAustinTX.JPG/220px-BobBullockMuseumAustinTX.JPG',
-              description: 'This is the museum that we chose. The Bullock has several floors available for weddings with a capacity for 300. The cost to reserve the ground floor is $4,500. For more information, call (512) 936-4632. Photo by WhisperToMe.',
+              description: 'This is the museum that we chose.'+'<b>'+'The Bullock has several floors available for weddings with a capacity for 300.'+</b>'+ 'The cost to reserve the ground floor is $4,500. For more information, call (512) 936-4632. Photo by WhisperToMe.',
               location: {
                   center: [-97.73915, 30.2803],
                   zoom: 17,

b3dcd3ab990645468b7029ed28db3689f260258d
AustinAmy <67663377+AustinAmy@users.noreply.github.com>
2020-09-08 16:33:26 -0500

    Update config.js

--- WeddingMuseum/config.js
+++ WeddingMuseum/config.js
@@ -7,13 +7,13 @@ var config = {
     title: 'The Three Best Museums in Austin for an Indoor Wedding',
     subtitle: 'During the summer, the temperature in Austin commonly tops 100 degrees. One affordable way to host your wedding indoors is to host it in a museum. While shopping for my own wedding venue, I narrowed my search to these three museums, based on their capacity and cost.',
     byline: 'By Amy Wu',
-    footer: 'Data Sources: The Knot.com; Blanton Museum of Art; Jones Center - The Contemporary Austin; Bullock Texas State History Museum; Mapbox',
+    footer: 'Data Sources: <a href="https://www.theknot.com/marketplace/wedding-reception-venues-austin-tx/museum">The Knot</a>; <a href="https://blantonmuseum.org/2016/01/choosing-the-blanton-museum-for-your-special-day-2/">Blanton Museum of Art</a>; <a href="http://thecontemporaryaustin.org/wp-content/uploads/2019/09/Jones_Center_Site_Rentals_2020.pdf">Jones Center - The Contemporary Austin</a>; <a href="https://www.thestoryoftexas.com/upload/files/Bullock-Museum-Rental-Rates-2020.pdf">Bullock Texas State History Museum</a>; <a href="https://demos.mapbox.com/scrollytelling/">Mapbox</a>',
     chapters: [
        {
             id: 'first-museum',
             title: 'Blanton Museum of Art',
             image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/University_of_Texas_at_Austin_August_2019_41_%28Jack_S._Blanton_Museum_of_Art%29.jpg/220px-University_of_Texas_at_Austin_August_2019_41_%28Jack_S._Blanton_Museum_of_Art%29.jpg',
-            description: 'The Blanton has capacity for 150. Cost starts at $5,000.For more information, call (512) 475-6516.Photo by Michael Barera.',
+            description: 'The Blanton has capacity for 150. Cost starts at $5,000.For more information, call (512) 475-6516. Photo by <a href="https://en.wikipedia.org/wiki/Blanton_Museum_of_Art">Michael Barera</a>.',
 
             location: {
                 center: [-97.73744, 30.281],
@@ -28,7 +28,7 @@ var config = {
             id: 'second-museum',
             title: 'Jones Center - The Contemporary Austin',
             image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/The_Contemporar_yAustin_-_JonesCenter.jpg/220px-The_Contemporar_yAustin_-_JonesCenter.jpg',
-            description: 'The Jones Center has capacity for 250. Cost starts at $2,900.For more information, call (512) 458-8191 ext. 255. Photo by Laguna Jones.',
+            description: 'The Jones Center has capacity for 250. Cost starts at $2,900.For more information, call (512) 458-8191 ext. 255. Photo by <a href="https://en.wikipedia.org/wiki/The_Contemporary_Austin">Laguna Jones</a>.',
             location: {
                 center: [-97.74269, 30.26958],
                 zoom: 17,
@@ -42,7 +42,7 @@ var config = {
               id: 'third-museum',
               title: 'Bullock Texas State History Museum',
               image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/BobBullockMuseumAustinTX.JPG/220px-BobBullockMuseumAustinTX.JPG',
-              description: 'This is the museum that we chose. The Bullock has several floors available for weddings with a capacity for 300. The cost to reserve the ground floor is $4,500. For more information, call (512) 936-4632. Photo by WhisperToMe.',
+              description: 'This is the museum that we chose. The Bullock has several floors available for weddings with a capacity for 300. The cost to reserve the ground floor is $4,500. For more information, call (512) 936-4632. Photo by <a href="https://en.wikipedia.org/wiki/Bullock_Texas_State_History_Museum">WhisperToMe</a>.',
               location: {
                   center: [-97.73915, 30.2803],
                   zoom: 17,
